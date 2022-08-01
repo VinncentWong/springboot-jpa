3ad9fa51234a4af77f2bbf6ce3462eb3708b7a84
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-01 11:19:45 +0700

    Add note

--- src/main/java/com/demo/SpringbootDataJpaHibernateApplication.java
+++ src/main/java/com/demo/SpringbootDataJpaHibernateApplication.java
@@ -9,5 +9,24 @@ public class SpringbootDataJpaHibernateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDataJpaHibernateApplication.class, args);
 	}
-
+	
+	/*
+	 * What is persistence data?
+	 * Persistence data is a data that can live outside application(once the application off, the data
+	 * would not lost -> out-memory)
+	 * 
+	 * When talking about persistence data in Java, it means about saving data into database using SQL
+	 * 
+	 * JPA(Java Persistence API) is specification that define
+	 * an API that manages persistence object and object-relational mapping
+	 * Hibernate is the implementation of JPA.
+	 * 
+	 * Data source is a connection that connect our service to database
+	 * It will connect through JDBC Driver
+	 * JDBC is a Java capabilities to connect to RDBMS(MySQL, PostgreSQL, etc.)
+	 * But it only provide the abstraction of the Database
+	 * To get the implementation of the database, we could use JDBC Driver to get not the abstraction
+	 * but the implementation
+	 *  
+	 */
 }
