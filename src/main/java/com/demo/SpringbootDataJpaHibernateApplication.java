45d8cf4b08321f65268e6aee6368e1307d51203f
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-01 10:37:51 +0700

    init file

---
+++ src/main/java/com/demo/SpringbootDataJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataJpaHibernateApplication.class, args);
+	}
+
+}
